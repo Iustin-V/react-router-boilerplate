c25387a34493376a9772a283726975a940efec3d
Iustin Vasiliu <iustinv2.0@gmail.com>
2025-11-13 14:25:42 +0200

    test

---
+++ server/app.ts
@@ -0,0 +1,7 @@
+// @ts-ignore
+import * as build from "virtual:react-router/server-build";
+import { createRequestHandler, RouterContextProvider } from "react-router";
+
+const handler = createRequestHandler(build);
+
+export default (req: Request) => handler(req, new RouterContextProvider() as any);
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,17 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { envOnlyMacros } from "vite-env-only";
 
-export default defineConfig({
+// export default defineConfig({
+//   plugins: [tsconfigPaths(), envOnlyMacros(), tailwindcss(), reactRouter()],
+//   build: {
+//     rollupOptions: isSsrBuild ? { input: "./server/app.ts" } : undefined,
+//   },
+//   server: { port: 3000 },
+// });
+export default defineConfig(({ isSsrBuild }) => ({
+  build: {
+    rollupOptions: isSsrBuild ? { input: "./server/app.ts" } : undefined,
+  },
   plugins: [tsconfigPaths(), envOnlyMacros(), tailwindcss(), reactRouter()],
-  server: { port: 3000 },
-});
+    server: { port: 3000 },
+}));
